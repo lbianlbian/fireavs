27e997dcb70333dc14a6a4f423d20a350738a2dc
Wesley Bian <wesleybian@gmail.com>
2025-02-25 20:26:50 -0800

    forgot to add form folder

---
+++ webapp/frontend-interface/app/form/form.js
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import MuiCard from '@mui/material/Card';
+import axios from "axios";
+import { styled } from '@mui/material/styles';
+import LinearProgress from '@mui/material/LinearProgress';
+
+import AppTheme from './styling/AppTheme';
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  //marginBottom: "2%",
+  [theme.breakpoints.up('sm')]: {
+    maxWidth: '450px',
+  },
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
+
+const SearchContainer = styled(Stack)(({ theme }) => ({
+  //height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+  minHeight: '100%',
+  padding: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(1),
+  },
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    }),
+  },
+}));
+
+export default function Form(props) {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const handleSubmit = async (event) => {
+    event.preventDefault();  // stop default refresh
+    setIsLoading(true);
+    setIsLoading(false);
+  };
+  return (
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+      <SearchContainer direction="column" justifyContent="space-between">
+        <Card variant="outlined">
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+          >
+            Is there a fire here?
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              gap: 2,
+            }}
+          >
+            <FormControl>
+              <TextField
+                id="lat"
+                name="lat"
+                placeholder="Lattitude"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={'primary'}
+              />
+              <TextField
+                id="long"
+                name="long"
+                placeholder="Longitude"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={'primary'}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+            >
+              Find out
+            </Button>
+          </Box>
+          {isLoading ? (<LinearProgress />) : (<></>)}
+        </Card>
+      </SearchContainer>
+      <Card variant="outlined">
+      </Card>
+    </AppTheme>
+  );
+}
